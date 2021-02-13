fb330f9138c71dc7f33e176f8302a9286ef6bd1f
Saoud <saoud@saoud.dev>
2021-02-12 16:26:20 -0800

    start to build out business logic

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+//Buisness Logic
+function roboRogers(number) {
+    const array = [];
+    let i = 0;
+
+    while (i<=number) {
+        array.push(i.toString());
+        i ++;
+    }
+}
+
+
+
+//UI Logic
