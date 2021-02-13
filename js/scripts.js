b5ec7fb374adc5770c62401884342c7ad710cf77
Saoud <saoud@saoud.dev>
2021-02-13 00:53:28 -0800

    adding exceptions to business logic

--- js/scripts.js
+++ js/scripts.js
@@ -8,16 +8,30 @@ function roboRogers(number) {
         i ++;
     }
 
+    
+		
+    if (array[index] === "13") {
+    return "Won't you be my neighbor?";
+    	} else if (array[index] === "21") {
+      return "Boop";
+      } else if (array[index] === "32"); {
+      return "Won't you be my neighbor?"
+      }
     let index = 0;
-
     for (index = 0; index < array.length; index++) {
-        if (array[index].includes(1)){
+        if (array[index].includes(13)){
+            array[index] = "Won't you be my neighbor!";
+        } else if (array[index].includes(21)){
+            array[index] = "Boop"
+        } else if (array[index].includes(32)){
+            array[index] = "Won't you be my neighbor?";
+        } else if (array[index].includes(1)){
             array[index] = "Beep!";
         } else if (array[index].includes(2)){
             array[index] = "Boop!"
         } else if (array[index].includes(3)){
             array[index] = "Won't you be my neighbor?";
-        } else (array[index] = array[index]);
+        else (array[index] = array[index]);
     }
 
     let string = "";
@@ -27,4 +41,14 @@ function roboRogers(number) {
     return string.slice(0,String.length - 1);
 }
 
+}
+
 //UI Logic
+$document.ready(function() {
+    $("formOne").submit(function(event) {
+        const number = $("input#number").val();
+        const output = roboRogers(parseInt(number));
+        $("#roboRogers").text(output);
+        event.preventDefault();
+    )};
+)};
