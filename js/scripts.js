765e0e44cc2dc5b2fe78c770cfbaef5247de1f19
Saoud <saoud@saoud.dev>
2021-02-12 17:31:42 -0800

    add more business logic

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,18 @@ function roboRogers(number) {
         array.push(i.toString());
         i ++;
     }
+
+    let index = 0;
+
+    for (index = 0; index < array.length; index++) {
+        if (array[index].includes(1)){
+            array[index] = "Beep!";
+        } else if array[index].includes(2){
+            array[index] = "Boop!"
+        } else if array[index].includes(3){
+            array[index] = "Won't you be my neighbor?";
+        } else array[index] = array[index];
+    }
 }
 
 
